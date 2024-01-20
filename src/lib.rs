c05d0dd49316a2569116a44b74af96ef08aee02c
OyuNet <oyunarda662@gmail.com>
2024-01-20 23:51:14 +0300

    Trigonometrik ifadeler.

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,12 @@ pub fn dividing(num1: f64, num2: f64) -> f64 {
     return num1/num2;
 }
 
-pub fn exponent(base: i32, power: i32) -> i32 {
-    let mut pwr = power - 1;
+pub fn exponent(base: f64, power: f64) -> f64 {
+    let mut pwr = power - (1 as f64);
     let mut result = base;
-    while pwr > 0 {
+    while pwr > 0 as f64 {
         result = result * base;
-        pwr = pwr - 1;
+        pwr = pwr - (1 as f64);
     }
 
     return result;
@@ -29,10 +29,34 @@ pub fn root(num: f64, power: f64) -> f64 {
     return num.powf(1 as f64 / power);
 }
 
-pub fn sqrt(num: i32) -> f64 {
-    return root(num as f64, 2 as f64);
+pub fn sqrt(num: f64) -> f64 {
+    return root(num, 2 as f64);
 }
 
-pub fn cbrt(num: i32) -> f64 {
+pub fn cbrt(num: f64) -> f64 {
     return root(num as f64, 3 as f64);
+}
+
+pub fn sinx(adjacent: f64, oppose: f64) -> f64 {
+    return oppose/sqrt(exponent(adjacent, 2 as f64) + exponent(oppose, 2 as f64));
+}
+
+pub fn cosx(adjacent: f64, oppose: f64) -> f64 {
+    return adjacent/sqrt(exponent(adjacent, 2 as f64) + exponent(oppose, 2 as f64));
+}
+
+pub fn tanx(adjacent: f64, oppose: f64) -> f64 {
+    return oppose/adjacent;
+}
+
+pub fn cotx(adjacent: f64, oppose: f64) -> f64 {
+    return adjacent/oppose;
+}
+
+pub fn secx(adjacent: f64, oppose: f64) -> f64 {
+    return (1 as f64)/cosx(adjacent, oppose);
+}
+
+pub fn cscx(adjacent: f64, oppose: f64) -> f64 {
+    return (1 as f64)/sinx(adjacent, oppose);
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use rust_calc;
 // I will try to write a basic calculator with selector. Good luck to me.
 fn main() {
     loop {
-        println!("Enter your desired option: \n1 - Summary\n2 - Subtraction\n3 - Multiplication\n4 - Dividing\n5 - Exponent\n6 - Square Root\n7 - Cube Root\n8 - Root\n9 - Exit");
+        println!("Enter your desired option: \n1 - Summary\n2 - Subtraction\n3 - Multiplication\n4 - Dividing\n5 - Exponent\n6 - Square Root\n7 - Cube Root\n8 - Root\n9 - Sinx\n10 - Cosx\n11 - Tanx\n12 - Cotx\n13 - Secx\n14 - Cosecx\n15 - Exit");
         let mut user_input = String::new();
 
         io::stdin().read_line(&mut user_input).expect("You wrote so wrong thing man...");
@@ -38,17 +38,17 @@ fn main() {
             "5" => {
                 let num1 = rust_input::int_input("Enter your exponent's base:");
                 let num2 = rust_input::int_input("Enter your exponent's power:");
-                let result = rust_calc::exponent(num1, num2);
+                let result = rust_calc::exponent(num1 as f64, num2 as f64);
                 println!("{}", result)
             },
             "6" => {
                 let num1 = rust_input::int_input("Enter your desired value:");
-                let result = rust_calc::sqrt(num1);
+                let result = rust_calc::sqrt(num1 as f64);
                 println!("{}", result)
             },
             "7" => {
                 let num1 = rust_input::int_input("Enter your desired value:");
-                let result = rust_calc::cbrt(num1);
+                let result = rust_calc::cbrt(num1 as f64);
                 println!("{}", result)
             },
             "8" => {
@@ -57,7 +57,43 @@ fn main() {
                 let result = rust_calc::root(num1 as f64, num2 as f64);
                 println!("{}", result)
             },
-            "9" => { println!("See you next time!"); break; },
+            "9" => {
+                let num1 = rust_input::int_input("Enter your adjacent:");
+                let num2 = rust_input::int_input("Enter your oppose:");
+                let result = rust_calc::sinx(num1 as f64, num2 as f64);
+                println!("{}", result);
+            },
+            "10" => {
+                let num1 = rust_input::int_input("Enter your adjacent:");
+                let num2 = rust_input::int_input("Enter your oppose:");
+                let result = rust_calc::cosx(num1 as f64, num2 as f64);
+                println!("{}", result);
+            },
+            "11" => {
+                let num1 = rust_input::int_input("Enter your adjacent:");
+                let num2 = rust_input::int_input("Enter your oppose:");
+                let result = rust_calc::tanx(num1 as f64, num2 as f64);
+                println!("{}", result);
+            },
+            "12" => {
+                let num1 = rust_input::int_input("Enter your adjacent:");
+                let num2 = rust_input::int_input("Enter your oppose");
+                let result = rust_calc::cotx(num1 as f64, num2 as f64);
+                println!("{}", result);
+            },
+            "13" => {
+                let num1 = rust_input::int_input("Enter your adjacent:");
+                let num2 = rust_input::int_input("Enter your oppose");
+                let result = rust_calc::secx(num1 as f64, num2 as f64);
+                println!("{}", result);
+            },
+            "14" => {
+                let num1 = rust_input::int_input("Enter your adjacent:");
+                let num2 = rust_input::int_input("Enter your oppose");
+                let result = rust_calc::cscx(num1 as f64, num2 as f64);
+                println!("{}", result);
+            }
+            "15" => { println!("See you next time!"); break; },
             _ => println!("Your entered number can't be found on list.")
         }
     }
